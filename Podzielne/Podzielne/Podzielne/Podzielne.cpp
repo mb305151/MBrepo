65d3d0726a45b2488d168f9b1a207c5cf477d36d
mb305151 <mb305151@student.polsl.pl>
2022-10-15 22:47:11 +0200

    Podzielne

---
+++ Podzielne/Podzielne/Podzielne/Podzielne.cpp
@@ -0,0 +1,20 @@
+﻿// Podzielne.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	int count = 0;
+	cin >> a >> b;
+	for (int i = a; i <= b; i++) {
+		if (i % 3 == 0 || i % 5 == 0) {
+			count++;
+		}
+	}
+	cout << count++<<"\n";
+}
+
