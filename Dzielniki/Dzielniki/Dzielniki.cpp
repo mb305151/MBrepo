09fd100c6b078fd1d5f8325b18e85b1a0d726b67
mb305151 <mb305151@student.polsl.pl>
2022-10-19 17:27:30 +0200

    new

---
+++ Dzielniki/Dzielniki/Dzielniki.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		if (n % i == 0 and i<1000000000000) {
+			cout << i << "\n";
+
+		}
+	return 0;
+
+}
